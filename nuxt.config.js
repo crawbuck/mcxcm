7db02112deb94603f076fff95d23fbf1d6bc8b1d
Michael Crawbuck <michael@brooklinen.com>
2022-09-11 17:56:27 -0400

    More meta shit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,79 @@
+const metaObj = () => {
+  return ([
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { hid: 'description', name: 'description', content: "Beginning at 12PM. We hope that you'll join us on the afternoon of November 27th to celebrate our engagement." },
+    { name: 'format-detection', content: 'telephone=no' },
+    { name: 'twitter:card', content: 'summary_large_image' },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: "Chelsey & Michael's Engagement Party",
+    },
+    { name: 'twitter:site', content: '@michaelcrawbuck' },
+    {
+      hid: 'twitter:description',
+      name: 'twitter:description',
+      content: "Beginning at 12PM. We hope that you'll join us on the afternoon of November 27th to celebrate our engagement.",
+    },
+    {
+      hid: 'twitter:image',
+      name: 'twitter:image',
+      content: './test.jpg',
+    },
+    {
+      property: 'twitter:image:alt',
+      content: 'test',
+    },
+
+    // Required type
+    { hid: 'og:type', property: 'og:type', content: 'website' },
+
+    // Required title
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: "Chelsey & Michael's Engagement Party",
+    },
+
+    // Required canonical url
+    {
+      hid: 'og:url',
+      property: 'og:url',
+      content: 'https://chelseyxmichael.wedding/',
+    },
+
+    // Required image (you can add an image parameter if you want to change images per page or read part 3 how we generate an image per page)
+    {
+      hid: 'og:image',
+      property: 'og:image',
+      content: './test.jpg',
+    },
+
+    // Optional image width
+    { property: 'og:image:width', content: '1200' },
+
+    // Optional image heigth
+    { property: 'og:image:height', content: '627' },
+
+    // Optional image alt text
+    {
+      property: 'og:image:alt',
+      content: 'test',
+    },
+
+    // Optional description
+    {
+      hid: 'og:description',
+      property: 'og:description',
+      content: "Beginning at 12PM. We hope that you'll join us on the afternoon of November 27th to celebrate our engagement.",
+    },
+  ])
+}
+
+
+
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -5,48 +81,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Chelsey & Michael's Engagement Party",
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "Beginning at 12PM. We hope that you'll join us on the afternoon of November 27th to celebrate our engagement." },
-      { name: 'format-detection', content: 'telephone=no' },
-      // Open Graph
-      {
-        hid: 'og:site_name',
-        property: 'og:site_name',
-        content: "Chelsey & Michael's Engagement Party"
-      },
-      {
-        hid: 'og:type',
-        property: 'og:type',
-        content: 'website'
-      },
-      {
-        hid: 'og:title',
-        property: 'og:title',
-        content: "Chelsey & Michael's Engagement Party"
-      },
-      {
-        hid: 'og:description',
-        property: 'og:description',
-        content: "Beginning at 12PM. We hope that you'll join us on the afternoon of November 27th to celebrate our engagement."
-      },
-      {
-        hid: 'og:image',
-        property: 'og:image',
-        content: '/test.jpeg'
-      },
-      {
-        hid: 'og:url',
-        property: 'og:url',
-        content: 'https://chelseyxmichael.wedding/'
-      },
-      {
-        hid: 't-type',
-        name: 'twitter:card',
-        content: 'summary_large_image'
-      },
-    ],
+    meta: metaObj(),
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
