3300559f8620b6db166b470379f890e6ad03dfe9
Michael Crawbuck <michael@brooklinen.com>
2022-10-04 19:19:40 -0400

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,12 @@
 <template>
   <main role="main" class="App" :class="{ 'App--Loaded' : loaded }">
-    <div>
+    <div :style="`min-height: calc(100vh - ${offset}px)`">
       <Header :content="header" />
       <Billboard :content="event" />
       <Form v-if="!submitted" :content="form" @formSubmitted="formSubmitted" />
       <ThankYou v-if="submitted" :content="form" :data="data" />
     </div>
-    <Footer />
+    <Footer ref="footer" />
   </main>
 </template>
 
@@ -35,11 +35,18 @@ export default {
     }
   },
   mounted() {
+    this.setOffset();
+    window.addEventListener("resize", () => {
+      this.setOffset();
+    });
     setTimeout(() => {
       return (this.loaded = true);
     }, 250);
   },
   methods: {
+    setOffset() {
+      return (this.offset = this.$refs.footer.$el.scrollHeight);
+    },
     formSubmitted(event) {
       this.submitted = !this.submitted;
       this.data = event;
